03d74ac97c89903af7bd83afd007f1658a044a8d
Maciej <Maciej>
2016-11-29 12:24:26 +0100

    tresc zadania

---
+++ MSI-Project1-v2/src/msi/project1/v2/service/PoissonStramCalc.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package msi.project1.v2.service;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Maciek
+ */
+public class PoissonStramCalc {
+    
+    public ArrayList calculatePropability(Integer[] lambdas,Integer[] secs,Integer time){
+        ArrayList<ArrayList> propability = new ArrayList<>();
+        for(Integer lambda:lambdas){
+            ArrayList<BigDecimal> perLamda = new ArrayList<>();
+            for(Integer sec:secs){
+                perLamda.add(new BigDecimal(String.valueOf(calculatePropability(lambda,sec,time))).setScale(4, BigDecimal.ROUND_HALF_UP));
+//                perLamda.add(calculatePropability(lambda,sec,time));
+            }
+            propability.add(perLamda);
+        }
+        return propability;
+    }
+
+    private BigDecimal calculatePropability(Integer lambda, Integer sec, Integer time) {
+        BigDecimal bLamda =  new BigDecimal(lambda);
+        BigDecimal bSec = new BigDecimal(sec);
+        BigDecimal bTime =  new BigDecimal(time);
+        BigDecimal temp = bLamda.multiply(bTime);
+        temp = temp.divide(factorial(bSec),MathContext.DECIMAL128);
+        temp = temp.multiply(new BigDecimal(Math.exp(Double.valueOf(lambda)*(-time))));
+        return temp;
+    }
+    
+    public BigDecimal factorial(BigDecimal n) {
+    BigDecimal factorial = BigDecimal.valueOf(1);
+    for (int i = 1; i <= n.intValue(); i++) {
+        factorial = factorial.multiply(BigDecimal.valueOf(i));
+    }
+    return factorial;
+} 
+}
---
+++ MSI-Project1-v2/src/msi/project1/v2/view/Window.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package msi.project1.v2.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+/**
+ *
+ * @author Maciek
+ */
+public class Window extends JFrame {
+
+    static final int witdh = 800;
+    static final int height = 600;
+
+    String[] names = {"lambda", "Pi"};
+    final JTable table = new JTable(getPropabilityTable(), names);
+    String[] namesDist = {"t", "lambda=2", "labmda=10"};
+    final JTable tableDist = new JTable(getComultativeDistributionTable(), namesDist);
+    JButton propButton = new JButton("Pokaz diagram prawdopodobienstwa blokady");
+    JButton distButton = new JButton("Pokaz diagram dystrybuanty");
+
+    JFreeChart jfc;
+
+    JPanel listPane = new JPanel();
+
+    public Window() throws HeadlessException {
+        setSize(witdh, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        propButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultCategoryDataset dcd = new DefaultCategoryDataset();
+                for (String[] s : getPropabilityTable()) {
+                    dcd.addValue((double) Double.valueOf(s[1]),"", s[0]);
+                }
+                jfc = ChartFactory.createLineChart("b)	Srednia liczbe zadan w kolejce dla wszystkich lambd ", "lambda", "Pi", dcd, PlotOrientation.VERTICAL, true, true, false);
+                CategoryPlot plot = jfc.getCategoryPlot();
+                plot.setRangeGridlinePaint(Color.black);
+                ChartFrame cf = new ChartFrame("Prawdopodobienstwo", jfc, true);
+                cf.setSize(800, 500);
+                cf.setVisible(true);
+            }
+        });
+//
+        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+        listPane.add(new JScrollPane(table), BorderLayout.CENTER);
+        listPane.add(propButton);
+
+//        distButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                DefaultCategoryDataset dcd = new DefaultCategoryDataset();
+//                for (String[] s : getComultativeDistributionTable()) {
+//                    dcd.addValue((double) Double.valueOf(s[1]), "Lambda = 2", s[0]);
+//                    dcd.addValue((double) Double.valueOf(s[2]), "Lambda = 10", s[0]);
+//                }
+//                jfc = ChartFactory.createBarChart3D("Wykres prawdopodobieństwa", "K", "F(t)", dcd, PlotOrientation.VERTICAL, true, true, false);
+//                CategoryPlot plot = jfc.getCategoryPlot();
+//                plot.setRangeGridlinePaint(Color.black);
+//                ChartFrame cf = new ChartFrame("Dystrybuanta", jfc, true);
+//                cf.setSize(800, 500);
+//                cf.setVisible(true);
+//            }
+//        });
+//        listPane.add(new JScrollPane(tableDist), BorderLayout.CENTER);
+//        listPane.add(distButton);
+        getContentPane().add(listPane);
+    }
+
+    private String[][] getPropabilityTable() {
+        String[][] table = {
+            {"0", "1.681210689"},
+            {"1", "5.041020712"},
+            {"2", "6.668385909"},
+            {"3", "7.500133297"},
+            {"4", "8.000016202"},
+            {"5", "8.333336057"},
+            {"6", "8.571429154"}//,
+//            {"7", "0.011882501429576000000000"},
+//            {"8", "0.041588755003516000000000"},
+//            {"9", "0.116448514009845000000000"},
+//            {"10", "0.244541879420674000000000"},
+//            {"11", "0.342358631188943000000000"},
+//            {"12", "0.239651041832260000000000"}//,
+//            {"12", "0.041353864"},
+//            {"13", "0.039699709"},
+//            {"14", "0.038111721"},
+//            {"15", "0.036587252"},
+//            {"16", "0.035123762"},
+//            {"17", "0.033718811"},
+//            {"18", "0.032370059"},
+//            {"19", "0.031075257"},
+//            {"20", "0.029832246"},
+//            {"21", "0.028638956"}
+        };
+        return table;
+    }
+
+    private String[][] getComultativeDistributionTable() {
+        String[][] table = {
+            {"0.1", "0.1813", "0.8647"},
+            {"0.2", "0.3297", "0.9502"},
+            {"0.3", "0.4512", "0.9817"},
+            {"0.4", "0.5507", "0.9933"},
+            {"0.5", "0.6321", "0.9975"},
+            {"0.6", "0.6988", "0.9991"},
+            {"0.7", "0.7534", "0.9991"},
+            {"0.8", "0.7981", "0.9997"},
+            {"0.9", "0.8347", "0.9999"},
+            {"1.0", "0.8647", "1.0000"},
+            {"1.1", "0.8892", "1.0000"},
+            {"1.2", "0.9093", "1.0000"},
+            {"1.3", "0.9257", "1.0000"}
+        };
+
+        return table;
+    }
+}
